e0fcfa5c028209543424913a86a8ee0364d2d7e8
Roman Levin <romanlevin@gmail.com>
2014-06-17 22:17:08 +0200

    First Ember page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.ember.variant = :development
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
